b7a00c6fadcb92f34c3b615fe02f5c4e57ce900a
Claudio Fernandez <cc.rr.ff.2014@gmail.com>
2023-05-05 23:22:20 -0300

    feat(frontend): add module shared

--- Frontend/TuPsi/src/app/app.module.ts
+++ Frontend/TuPsi/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from './shared/shared.module';
 import { HomeModule } from './home/home.module';
 import { AuthModule } from './auth/auth.module'
 
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module'
     AppRoutingModule,
     HomeModule, 
     AuthModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
