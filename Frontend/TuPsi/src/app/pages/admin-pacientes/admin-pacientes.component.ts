bd359b90093e43ce63b803b101d746ee9de296d5
gabyselig <gabyseligmann@gmail.com>
2023-05-30 20:16:32 -0300

    subo el adminProfesionales

---
+++ Frontend/TuPsi/src/app/pages/admin-pacientes/admin-pacientes.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PacienteService } from 'src/app/services/paciente.service';
+import { Paciente } from 'src/app/models/paciente.model';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-admin-pacientes',
+  templateUrl: './admin-pacientes.component.html',
+  styleUrls: ['./admin-pacientes.component.css']
+})
+export class AdminPacientesComponent implements OnInit, OnDestroy {
+
+  Pacientes: Paciente[] = [];
+  pacienteCreadoSubscription: Subscription = Subscription.EMPTY;
+  tienePacientes: boolean = false;
+
+  constructor(private servicioPacientes: PacienteService) {}
+
+  ngOnInit() {
+    this.verPacientes();
+    this.pacienteCreadoSubscription = this.servicioPacientes
+      .getPacienteCreadoObservable()
+      .subscribe((paciente) => {
+        this.Pacientes.push(paciente);
+        this.tienePacientes = true;
+      });
+  }
+
+  ngOnDestroy() {
+    this.pacienteCreadoSubscription.unsubscribe();
+  }
+
+  verPacientes() {
+    this.servicioPacientes.getPaciente().subscribe(
+      (data) => {
+        this.Pacientes = data;
+        this.tienePacientes = this.Pacientes.length > 0;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  editarPaciente(item: Paciente) {
+    item.editando = true;
+  }
+
+  cancelarEdicion(item: Paciente) {
+    item.editando = false;
+  }
+
+  guardarCambios(item: Paciente) {
+    this.servicioPacientes.updatePaciente(item).subscribe(
+      (pacienteActualizado) => {
+        item.editando = false;
+        console.log('Paciente actualizado:', pacienteActualizado);
+      },
+      (error) => {
+        console.log('Error al actualizar el paciente:', error);
+      }
+    );
+  }
+
+  eliminarPaciente(id: number) {
+    this.servicioPacientes.deletePaciente(id).subscribe(
+      () => {
+        console.log('Paciente eliminado con ID:', id);
+        this.verPacientes();
+      },
+      (error) => {
+        console.log('Error al eliminar el paciente:', error);
+      }
+    );
+  }
+}
